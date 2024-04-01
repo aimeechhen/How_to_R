92e6e1161385de7d10eb8c0ddbfcaaabf92b6a72
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-04-01 00:19:57 -0700

    initial commit of various crs projection types and their formats based on what package or reference code for them

---
+++ spatial/scripts/crs projection list.R
@@ -0,0 +1,33 @@
+
+#BC Albers projection
+
+# NAD83 / BC Albers
+bc_albers <- "ESRI:3005"
+bc_albers <- "EPSG:3005"
+
+library(bcmaps)
+transform_bc_albers()
+
+
+
+#..........................................
+
+# Lat/long
+# WGS 84 /
+"+proj=longlat +datum=WGS84"
+#EPSG:4326
+
+wgs84 <- "EPSG:4326"
+
+
+
+
+#..........................................
+
+#Wintri projection system
+crs_wintri <- "ESRI:53018" 
+
+epsg_53018 <- crs("+init=epsg:53018")
+
+# Project the spatvector object to EPSG:53018
+china_projected <- project(china, epsg_53018)
