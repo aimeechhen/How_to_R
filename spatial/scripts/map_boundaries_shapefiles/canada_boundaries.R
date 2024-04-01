33d42323b24202737285db54ddecfddfaa9121e0
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-04-01 00:12:21 -0700

    initial commit how to download and plot canada boundaries, canada + USA boundaries

---
+++ spatial/scripts/map_boundaries_shapefiles/canada_boundaries.R
@@ -0,0 +1,54 @@
+
+
+
+
+
+#.........................................................
+# Base R plot (Canada+BC)
+#.........................................................
+
+library(geodata)
+#define the projection system
+crs_wintri <- "ESRI:53018"  
+
+#download boundaries
+provinces <- gadm(country="Canada", level=1, path = tempdir()) #include provinces border
+
+#subset to bc boundaries
+bc <- provinces[provinces$NAME_1 %in% "British Columbia", ]
+
+#reproject
+bc_wintri <- project(bc, crs_wintri)
+
+# plot bc
+plot(bc)
+
+
+
+
+#..............................................................
+# Base R: canada + us ----
+#..............................................................
+
+library(geodata)
+#level 0 = country; level 1 = province/state; level 2 = counties
+provinces <- gadm(country="Canada", level=1, path = tempdir())
+states <- gadm(country="USA", level=1, path = tempdir())
+
+#plot both shape files, layered
+plot(provinces)
+plot(states, add = TRUE)
+
+#Check names
+CanUS <- rbind(states, provinces)
+print(unique(CanUS$NAME_1))
+
+#https://stackoverflow.com/questions/10763421/r-creating-a-map-of-selected-canadian-provinces-and-u-s-states
+
+# OR
+#https://plantarum.ca/2023/02/13/terra-maps/
+
+CanUS <- rbind(states, provinces)
+plot(CanUS, xlim = c(-180, -50), border = "darkgrey", col = "grey")
+plot(CanUS[CanUS$NAME_1 %in% "British Columbia", ], border="black", 
+     col="white", add=TRUE)
