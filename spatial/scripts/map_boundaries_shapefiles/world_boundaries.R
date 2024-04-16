3ff2288ed1357dc97a0bb50a245f530d8c713fff
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-04-16 09:49:00 -0700

    moved laurens world and china to own script

--- spatial/scripts/map_boundaries_shapefiles/world_boundaries.R
+++ spatial/scripts/map_boundaries_shapefiles/world_boundaries.R
@@ -15,9 +15,10 @@ library(terra)
 library(sf)
 library(geodata)
 
+#level 0 = country; level 1 = province/state; level 2 = counties
+#change path = "filepath location" to save the shapefile
 canada <- gadm(country="Canada", level=0, path = tempdir())
 provinces <- gadm(country="Canada", level=1, path = tempdir())
-
 
 
 #define the projection system
@@ -36,7 +37,6 @@ provinces_wintri <- project(provinces, crs_wintri)
 
 # Convert to spatvector class
 world_wintri <- vect(world_wintri) 
-
 
 png(file="Figures/world_bc_map.png",
     width=6.86, height=3, units="in", res=600)
@@ -61,50 +61,4 @@ plot(provinces_wintri[provinces_wintri$NAME_1 %in% "British Columbia", ], border
      col="#de2d26", add=TRUE)
 dev.off()
 
-
-
-#.......................................................
-# Lauren's China ----
-#......................................................
-
-library(terra)
-library(sf)
-library(geodata)
-china <- gadm(country= "China", level=0, path = tempdir())
-#define the projection system
-crs_wintri <- "ESRI:53018" 
-#Reproject
-china <- project(china, crs_wintri)
-#plot
-plot(china)
-
-
-
-#.......................................................
-# ggplot: Lauren's Map (World) ----
-#......................................................
-
-library(sf)
-library(ggplot2)
-library(geodata)
-
-#res = 1 and 5 indicating the level of detail. 1 is high 5 is low
-world <- world(resolution=5, level=0, path = tempdir(), version="latest")
-
-# Convert to sf object
-world_sf <- st_as_sf(world)
-
-#define the projection system
-crs_wintri <- "ESRI:53018" 
-
-# Convert CRS to lat/long projection
-world_sf <- st_transform(world_sf, crs = crs_wintri)
-st_crs(world_sf)
-
-#import my data
-mp_coord <- read.csv("C:/Users/Pokedex/Downloads/global_coords.csv")
-
-ggplot() + 
-geom_sf(data = world_sf, fill = NA, color = "darkgrey", size = 1) +
-  geom_point(data = mp_coord, aes(x = long, y = lat), color = "black")
 
