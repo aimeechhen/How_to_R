0eca2f1114b544ab68a02cd7a3906c965faa6512
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-06-07 16:53:27 -0700

    added how to define the projection, added NOES GOES crs placeholder

--- spatial/scripts/crs projection list.R
+++ spatial/scripts/crs projection list.R
@@ -1,5 +1,14 @@
 
-#BC Albers projection
+
+# Define the projection of a vector
+proj4string(data) <- crs()
+
+# Set crs
+crs(x) <- crstype
+
+
+#..........................................
+# BC Albers projection ----
 
 # NAD83 / BC Albers
 bc_albers <- "ESRI:3005"
@@ -10,29 +19,24 @@ transform_bc_albers()
 
 
 #..........................................
+# Canada Albers Equal Area Conic projection ----
 
-# Canada Albers Equal Area Conic projection
 ca_albers <- "ESRI:102001"
 ca_albers <- "EPSG:102001"
 
 
-
-
 #..........................................
+# Lat/long projection ----
 
-# Lat/long
+
 # WGS 84 /
 "+proj=longlat +datum=WGS84"
-#EPSG:4326
-
-wgs84 <- "EPSG:4326"
-
-
+lat_long <-  "EPSG:4326"
 
 
 #..........................................
+# Wintri projection system ----
 
-#Wintri projection system
 crs_wintri <- "ESRI:53018" 
 
 epsg_53018 <- crs("+init=epsg:53018")
@@ -42,3 +46,38 @@ china_projected <- project(china, epsg_53018)
 
 
 
+#...........................................................
+# NOAA GOES satellite crs (Maybe, need to double check, place holder for now)
+goes_crs <- "PROJCRS[\"Geostationary_Satellite\", 
+    BASEGEOGCRS[\"GRS 1980(IUGG)\", 
+        DATUM[\"GRS 1980(IUGG)\", 
+            ELLIPSOID[\"GRS80\", 6378137, 298.257222101]], 
+        PRIMEM[\"Greenwich\", 0.0]], 
+    CONVERSION[\"Geostationary Satellite (Sweep X)\", 
+        METHOD[\"Geostationary Satellite (Sweep X)\", ID[\"EPSG\", 9838]], 
+        PARAMETER[\"Longitude of natural origin\", -137.0], 
+        PARAMETER[\"Satellite Height\", 35786023.0], 
+        PARAMETER[\"False easting\", 0.0], 
+        PARAMETER[\"False northing\", 0.0]], 
+    CS[Cartesian, 2], 
+        AXIS[\"(X)\", east], 
+        AXIS[\"(Y)\", north], 
+    USAGE[ 
+        SCOPE[\"unknown\"], 
+        AREA[\"World\"], 
+        BBOX[-90, -180, 90, 180]]]"
+
+# NOAA GOES satellite crs wkt
+goes_wkt <- 'PROJCRS["Geostationary_Satellite",
+    BASEGEOGCRS["GRS 1980",
+        DATUM["GRS_1980",
+            ELLIPSOID["GRS 1980",6378137,298.257222101]]],
+    CONVERSION["Geostationary Satellite (Sweep X)",
+        METHOD["Geostationary Satellite (Sweep X)", ID["EPSG", 9838]],
+        PARAMETER["Longitude of natural origin",-137, ANGLEUNIT["degree",0.0174532925199433], ID["EPSG",8802]],
+        PARAMETER["Satellite Height",35786023, LENGTHUNIT["metre",1], ID["EPSG",9001]],
+        PARAMETER["False easting",0, LENGTHUNIT["metre",1], ID["EPSG",8806]],
+        PARAMETER["False northing",0, LENGTHUNIT["metre",1], ID["EPSG",8807]]],
+    CS[Cartesian,2],
+        AXIS["(X)",east, ORDER[1], LENGTHUNIT["metre",1]],
+        AXIS["(Y)",north, ORDER[2], LENGTHUNIT["metre",1]]]'
