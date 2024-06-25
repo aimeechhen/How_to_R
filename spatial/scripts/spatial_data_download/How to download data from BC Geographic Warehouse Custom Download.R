f233cededd5720d2b7e28327d9dcf8b12c30a30b
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-06-25 12:16:06 -0700

    added instructions/details on how to download from BC Geographic Warehouse Custom Download source

---
+++ spatial/scripts/spatial_data_download/How to download data from BC Geographic Warehouse Custom Download.R
@@ -0,0 +1,10 @@
+
+
+#BC Geographic Warehouse Custom Download
+#Select 'Access/Download'
+#Enter order details:
+#Coordinate System = BC Albers (m)
+#Format = ArcView Shape (if you want .shp file, CSV doesn't give you want you want, i.e. geometries, coordinates etc.)
+#Area of Interest = None
+#Submit and then download via link that is sent to your email address
+#Extract zip file to get .shp file
