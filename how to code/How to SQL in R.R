43e18f2d2ad4a431995bc8e3b3abdca415ecfdda
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-09-11 11:13:51 -0700

    added how to sql basic packages used

---
+++ how to code/How to SQL in R.R
@@ -0,0 +1,19 @@
+
+# How to SQL in R
+
+# https://posit.co/blog/working-with-databases-and-sql-in-rstudio/
+
+
+# Task: how to make an SQL database to reorganize data and create a new database
+
+# Install necessary packages
+install.packages("DBI")
+install.packages("RSQLite")
+install.packages("RPostgres")
+
+
+
+# load packages
+library(DBI)
+library(RSQLite)
+library(RPostgres)
