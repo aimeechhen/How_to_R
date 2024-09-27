8b0f912c5c43ef8474e534966884f757004dd011
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-09-26 18:17:38 -0700

    added how to python via git bash commands

---
+++ how to git it on/how to python.R
@@ -0,0 +1,15 @@
+
+
+
+# install python via microsoft store (via python website or python.exe caused issues/was not detected?)
+
+# check version
+python --version
+pip --version
+
+# install pip3 via git bash through python
+python -m pip
+
+
+
+library(reticulate)
