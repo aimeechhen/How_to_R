04b93792020b6c7e00f108d05486bc9c906b772a
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-10-04 16:54:13 -0700

    initial commit of how to download data from gov of canada or bc gov instructions and links

---
+++ how to code/how to download government data.R
@@ -0,0 +1,36 @@
+
+
+
+
+# government of canada open government open data ----
+# https://open.canada.ca/data/dataset/
+# search what you want
+# select data
+# scroll down then select the data/resource you want, it will direct you to download
+# file type is stated
+
+# bc fire can also be accessed via open government via search
+
+
+
+
+
+#________________________________________________________
+# bc government data catalogue ----
+# https://catalogue.data.gov.bc.ca/datasets/
+
+
+#BC Geographic Warehouse Custom Download
+#Select 'Access/Download'
+#Enter order details:
+#Coordinate System = BC Albers (m)
+#Format = ArcView Shape (if you want .shp file, CSV doesn't give you want you want, i.e. geometries, coordinates etc.)
+#Area of Interest = None
+#Submit and then download via link that is sent to your email address
+#Extract zip file to get .shp file
+
+
+# bc fire
+# https://catalogue.data.gov.bc.ca/dataset/bc-wildfire-fire-perimeters-current
+# https://catalogue.data.gov.bc.ca/dataset/bc-wildfire-fire-perimeters-historical
+# https://www.for.gov.bc.ca/ftp/HPR/external/!publish/Maps_and_Data/Maps/Organizational_Overview/
