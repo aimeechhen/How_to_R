543dd76c28502dd9fc9e3b852fcbbab641aa5b09
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-12-10 11:22:26 -0800

    added how to rasterize a sf object to raster

--- how to code/convert sf to spatraster (rasterize).R
+++ how to code/convert sf to spatraster (rasterize).R
@@ -14,3 +14,21 @@ raster_template = rast(ext(ext_value),
 
 # convert geometries into vector object (i.e into a spatvector object) then convert spatvector into a spatraster object using the spatial properties outlined in the template
 raster_perimeter <- rasterize(vect(perimeter), raster_template)
+
+
+
+
+
+# first create a raster
+
+
+# get the outline of the polygons
+outline <- st_boundary(perimeter)
+# convert geometries into vector object (i.e into a spatvector object) 
+outline_spatvector <- vect(outline)
+# extract the timestamp for the current perimeter
+capture_timestamp <- FOIPPA$timestamp[i]
+# assign the timestamp
+outline_spatvector$timestamp <- capture_timestamp
+# then convert spatvector into a spatraster object using the spatial properties outlined above and set background cells/pixels in the raster to 0 so the raster contains only values of 0 or 1 (0 = outside the polygon, 1 = inside the polygon) via field and background arguments
+r <- rasterize(outline_spatvector, raster_template, field = "timestamp", background = 0)
