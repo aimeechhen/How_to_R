d7dc8bebb4c7bcc61310fd4c106e0d3a1067f4f6
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-12-10 11:33:30 -0800

    initial commit of git bash basic commands

---
+++ how to git it on/how to git bash.R
@@ -0,0 +1,18 @@
+
+# Git bash
+
+# set working directory
+#open the folder of the working directory
+#right click anywhere in the folder
+#select 'Git Bash Here'
+
+
+
+# go to your main branch and 2. merge the other branch to your main, then 3. move files into the directory you want, 4. commit 5. push
+
+
+git checkout main
+git merge other-branch
+git mv * directory/
+git commit -m "commit message"
+git push origin main 
