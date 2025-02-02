5ad2d604119ed01ba4b51713ac7c53de802c12ef
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2025-02-02 13:13:54 -0800

    added rmarkdown theme info

--- how to rmarkdown/how to rmarkdown.R
+++ how to rmarkdown/how to rmarkdown.R
@@ -4,17 +4,31 @@
 # https://stackoverflow.com/questions/69327328/r-markdown-to-pdf-causes-lines-to-overflow-off-page/69387188#69387188
 
 
----
-output: pdf_document
-header-includes:
-  - |
-    ```{=latex}
-    \usepackage{fvextra}
-    \DefineVerbatimEnvironment{Highlighting}{Verbatim}{
-      showspaces = false,
-      showtabs = false,
-      breaklines,
-      commandchars=\\\{\}
-    }
-    ```
----
+# ---
+# output: pdf_document
+# header-includes:
+#   - |
+#     ```{=latex}
+#     \usepackage{fvextra}
+#     \DefineVerbatimEnvironment{Highlighting}{Verbatim}{
+#       showspaces = false,
+#       showtabs = false,
+#       breaklines,
+#       commandchars=\\\{\}
+#     }
+#     ```
+# ---
+
+
+
+
+
+
+#Rmarkdown themes ----
+
+# Preview: https://rstudio4edu.github.io/rstudio4edu-book/rmd-themes.html#rmd-themes
+# "bootstrap", "cerulean", "cosmo", "darkly", "flatly", "journal", "lumen", "paper", "readable", "sandstone", "simplex", "spacelab", "united", "yeti"
+
+
+
+# https://fontawesome.com/icons
