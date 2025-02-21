a98d47e33383ff19a8b2eef654e4f9f233699ab1
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2025-02-21 12:16:15 -0800

    moved code chunk to how to spatial carpentry

--- how to code/2 how to spatial functions.R
+++ how to code/2 how to spatial functions.R
@@ -28,6 +28,7 @@ write_sf() #save specialized/more complicated objects
 st_geometry(df) <- NULL # remove geometry, coerce to data.frame
 sf <- st_set_geometry(df, sfc) # set geometry, return sf, (sfc = simple feature geometry list column)
 st_set_geometry(sf, NULL) # remove geometry, coerce to data.frame
+st_centroid(st_union(df)) # find the center of the data (point data, eg. gps locations)
 
 
 #____________________________________________________________________
@@ -73,27 +74,3 @@ add(x) <- value #add in place a SpatRaster to another SpatRaster object, compara
 x <- unwrap(x)
 
 
-
-#////////////////////////////////////////////////////////
-
-# how to extract raster values based on polygon
-#reproject the CRS to a geographic coordinate system (e.g., WGS 84)
-FOIPPA <- st_transform(FOIPPA, crs = st_crs("epsg:4326"))
-#get bounding box
-bbox <- st_bbox(FOIPPA)
-
-#Get elevation raster for the region(s) of interest (units = meters)
-dem_raster <- get_elev_raster(locations = FOIPPA, 
-                              z = 9,
-                              clip = 'bbox',
-                              expand = 0.2)
-raster::plot(dem_raster)
-
-# crop and mask elevation raster to extract values within boundaries
-cropped_dem <- crop(dem_raster, FOIPPA)
-masked_dem <- mask(cropped_dem, FOIPPA)
-
-# extract elevation values
-el_value <- as.data.frame(masked_dem, xy = TRUE, na.rm = TRUE)
-# rename columns
-colnames(el_value) <-  c("long", "lat", "el")
