e08e816a8007f37d64afdf580b8d12cc201bfa7c
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2025-02-25 14:11:53 -0800

    added how to add ggplot items to movevis maps

--- how to plot/How to moveVis.R
+++ how to plot/How to moveVis.R
@@ -16,6 +16,9 @@ move_data <- df2move(goat_data,
                      x = "location.long", y = "location.lat", time = "timestamp",
                      track_id = "goat_id")
 
+# if you need to transform, must use sp as st_transform() doesnt work for movestack objects
+move_data <- sp::spTransform(move_data, crs("+init=epsg:4326"))
+
 # align move_data to a uniform time scale (may take a while)
 m <- align_move(move_data, res = 120, unit = "mins")
 
@@ -30,9 +33,38 @@ frames <- frames_spatial(m, path_colours = rainbow(10)) %>% #,
 
 frames[[100]] # preview one of the frames, e.g. the 100th frame
 
+# preview a frame and adding a customization to test
+frames[[100]] +
+geom_path(aes(x = x, y = y), data = cathedral_df,
+          colour = "black", linetype = "dashed", linewidth = 1.5)), data = cathedral_df)
+
+
 # animate frames (may take a while, like hours, for a lot of data)
 # animate_frames(frames, out_file = "figures/moveVis.gif")
 Start <- Sys.time()
 animate_frames(frames, out_file = "figures/moveVis.mp4")
 # Approx animation duration = ~750.25s (~12.5 min) @ 25 fps for 18757 frames
 End <- Sys.time()
+
+
+
+
+# adding ggplot items such as rasters, shapefiles, ggplot customizations etc
+
+#shapefile
+add_gg(gg = expr(
+geom_sf(data = cathedral, color = "black", fill = NA, linewidth = 1, linetype = "dashed")), data = cathedral) %>%
+
+  #shapefile converted into points, dataframe object
+  add_gg(gg = expr(
+    geom_path(aes(x = x, y = y), data = cathedral_df,
+              colour = "black", linetype = "dashed", linewidth = 1.5)), data = cathedral_df) %>%
+  
+  # ggplot customizations
+  add_gg(expr(theme(legend.title = element_blank()))) %>% 
+  add_gg(expr(guides(linetype = "none"))) %>% 
+  
+  # adding a static text to the plot
+  add_text("Cathedral Provincial Park Boundary", 
+           x = -120.07, y = 49.003, #based on coordinates because the plot was based on coordinates for x and y
+           colour = "black", size =2)
