7b0b88a4293c8c9e538b1262f3ec4381cb46c576
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2025-04-20 12:23:57 -0700

    added git basic commands and troubleshooting github repo name change

--- how to git it on/how to git bash.R
+++ how to git it on/how to git bash.R
@@ -1,6 +1,13 @@
 
 # Git bash
 
+# basic commands
+cd # change directory
+ls #list content in working directory
+pwd #print working directory
+
+
+
 # set working directory
 #open the folder of the working directory
 #right click anywhere in the folder
@@ -16,3 +23,26 @@ git merge other-branch
 git mv * directory/
 git commit -m "commit message"
 git push origin main 
+
+
+
+# check files in directory
+ls
+
+
+# if you changed github repo location such as transferring repo from personal to lab repo, how to update for github desktop and rstudio if you're having issues
+cd '\path\to\repo'
+#check repo location
+git remote -v
+# if it is showing your old repo github url, update it
+git remote set-url origin new_url
+#check again to see if it has been updated
+git remote -v
+# if github isnt recognizing the repo and still says cannot locate, check status, if repo is being recognized by git
+git status
+# if you see fatal: not a git repository, then need to reinitialize Git
+git init
+# add repo again manually
+git remote add origin url_of_repo
+
+# if it doesnt work, just clone the repo into like a subfolder, then copy and paste the new .git folder into the original repo folder to override the old git info (lol) then delete the subfolder
