818dbb2f09382d096584a4854f4256524a41613a
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2025-04-20 12:22:37 -0700

    added script of rstudio info such as r version, package version, packages used, citations for packages etc.

---
+++ how to code/how to check r version, package, get package citation etc.R
@@ -0,0 +1,34 @@
+
+# how to rstudio info
+
+
+R.version # get R version
+sessionInfo() # get R session info
+packageVersion("package") # get r package version
+citation("package") # get citation for r package
+
+
+# analyses
+packageVersion("ctmm")
+packageVersion("lme4")
+
+# visualization
+
+
+
+#spatial
+packageVersion("sf")
+citation("sf")
+packageVersion("terra")
+citation("terra")
+
+
+
+packageVersion("")
+packageVersion("")
+packageVersion("")
+packageVersion("")
+
+
+# To see these entries in BibTeX format, use 'print(<citation>, bibtex=TRUE)', 'toBibtex(.)', 
+
