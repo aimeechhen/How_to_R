1d12204a510b3512d743fcad826b41e9aa54f138
Chhen <aimee.chhen@ubc.ca>
2025-11-27 13:40:12 -0800

    cleaned up plot base r script and added a few more arguments

--- how to plot/1 base r plots.R
+++ how to plot/1 base r plots.R
@@ -18,20 +18,24 @@ png(file = "figures/figure1.png", width = 6.86, height = 6, units = "in", res = 
 par(mfrow=c(2,1))                    # plot arrangement
 par(mgp = c(2, 0.5, 0))             #Adjust the third element (margin for axis title spacing)
 par(mar = c(3, 3, 1.25, 0.25))      #margin defaults (order: bottom, left, top, and right)
-plot(AKDE_1, 
-     col.DF = COL_1,        #Color option for the density function. Can be an array. Note: may give you errors when trying to plot UD, use 'col.UD= '
-     col.UD = COL_1,         #Color option for UDs
-     col.level = COL_1, 
+
+plot(df, 
+     col.DF = col_palette,        #ctmm::plot() argument: Color option for the density function. Can be an array. Note: may give you errors when trying to plot UD, use 'col.UD= '
+     col.UD = col_palette,         #ctmm::plot() argument: Color option for UDs
+     col.level = col_palette, 
      col.grid = NA, 
      col = rainbow(49),
      col = viridis(5), 
+     cex = ,                  # shape size 
      las = 2,                 # rotate axis label 
-     pch = ,                
+     pch = ,                  # marker shape
      level = NA,
      lwd.level = 1,            #line thickness
      lwd = 3,                  #line thickness
      #font=2,                  #bold axis text
      font.lab = 2,             #bold axis labels
+     adj = 0,                  #location of title (0 = left align)
+     side = 2,                 # location of title wrt to the panel, 2 = left side of the plot
      cex.lab = 1,              #size of axis title
      cex.axis = 0.8,           #size of axis text font
      axes = FALSE,             #remove all axes
@@ -39,7 +43,7 @@ plot(AKDE_1,
      xlab = "",                #remove x-axis label
      yaxt="n",                 #remove y-axis text
      xaxt="n",                 #remove x-axis text
-     main = "Christoffer and Kyle") #add title
+     main = "Title") #add title
 title("C", adj = 0)
 
 # modify axis titles, ensure you remove them first, see above
@@ -47,7 +51,7 @@ title("C", adj = 0)
 # yaxis: side = 2
 axis(side = 2, at = seq(0,9000,by = 1000)) #manually add axis tick labels 
 axis(side = 2, las = 2) # Rotate the axis labels (title).
-legend("topleft", legend = c("label 1", "label 2"))
+legend(legend = c("item1", "item2"))
 abline(0,0, col = "red") 
 
 #add another layer to the plot
